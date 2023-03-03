51bfbcad117b14aff2046504199e42b6d7c85513
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-03-03 23:43:33 +0000

    background set up

---
+++ src/containers/TilesHeader.js
@@ -0,0 +1,46 @@
+import anime from 'animejs/lib/anime.es.js';
+import { useState } from 'react';
+
+const TilesHeader = () => {
+    
+    let [columns, setColumns] = useState(Math.floor(document.body.clientWidth / 50));
+    let [rows, setRows] = useState(Math.floor(document.body.clientHeight / 50));
+
+    window.onresize = () => {
+        setColumns(Math.floor(document.body.clientWidth / 50));
+        setRows(Math.floor(document.body.clientHeight / 50))
+    }
+    window.onload = () => {
+        setColumns(Math.floor(document.body.clientWidth / 50));
+        setRows(Math.floor(document.body.clientHeight / 50))
+    }
+    
+    const handleOnClick = index => {
+  
+        anime({
+            targets: ".tile",
+            scale: [{value: 0.5, duration: 500},
+            {value: 1, duration: 500}],
+            delay: anime.stagger(100, {
+                grid: [columns, rows],
+                from: index
+            })
+        })
+    }
+
+    return ( 
+        <div 
+            style={{
+                display: 'grid',
+                gridTemplateColumns: `repeat(${columns}, 1fr)`,
+                gridTemplateRows: `repeat(${rows}, 1fr)`,
+            }}
+            className={`tiles h-[100vh] w-[100vw]`}>
+            {Array.from(Array(columns * rows)).map((tile, index) => {
+                return <div className="tile" onClick={(e) => handleOnClick(index)}></div>;
+            })}
+        </div>
+    );
+}
+ 
+export default TilesHeader;
