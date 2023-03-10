317ad72e6210bbb18aa6a43a38798f1e6f8d9b34
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-03-10 00:09:09 +0000

    completed about section timeline

--- src/containers/TilesHeader.js
+++ src/containers/TilesHeader.js
@@ -292,7 +292,7 @@ const TilesHeader = () => {
             className={`tiles h-[100vh] w-[100vw]`}>
             {Array.from(Array(columns * rows)).map((tile, index) => {
                 return <div className={`tile group
-                    ${pageState === "about" && index === columns * 4 + 1 ? "z-[10]" : "z-[0] scatter"}
+                    ${pageState === "about" && (index === columns * 4 + 1 ||  index === columns * (3 + Math.ceil((rows - 4) / 2)) + 1) ? "z-[10]" : "z-[0] scatter"}
                     ${splashState ? "before:inset-[0px] border-[rgb(40,40,40)] border-[0.5px] before:bg-[rgb(20,20,20,0.99)]" : "before:inset-[0.5px] before:bg-[rgb(20,20,20,0.8)] z-[0]"}`} 
                     onClick={(e) => selectAction(index)}
                 >
@@ -351,8 +351,8 @@ const TilesHeader = () => {
                     
                     <div 
                         style={{
-                            width:`${document.body.clientWidth - sizeMultiplier * 2}px`,
-                            height:`${document.body.clientWidth >= 560 ? sizeMultiplier * 4.25 : sizeMultiplier * 7}px`,
+                            width:`${document.body.clientWidth - sizeMultiplier * 2 - 2}px`,
+                            height:`${document.body.clientWidth >= 560 ? sizeMultiplier * ((rows-4)/2 - 0.75) : sizeMultiplier * ((rows-4)/2 - 0.75)}px`,
                             overflowY: 'scroll'
                         }}
                         className={`${textVanish ? 'about-animateDisappear-0ms' : "about-delay-0ms"} aboutbox text-white font-["Orbitron"]  tracking-normal md:tracking-[8px] text-sm md:text-md opacity-100 top-[50%] left-[0%] bg-[rgb(20,20,20,0.8)] pl-[18px] border-[var(--g3)] border-[1px] rounded-sm ${document.body.clientWidth <= 560 ? `translate-y-[-16px]` : `translate-y-[-26px]`} p-5 absolute overflow-visible`}
@@ -382,6 +382,94 @@ const TilesHeader = () => {
                             <CyberTooltip TransitionComponent={Zoom} placement="bottom" title="PostgreSQL"><p><SiPostgresql/></p></CyberTooltip>
                         </p> 
                         
+                    </div>
+                    
+                    
+                    : ""} 
+
+                    {!splashState && pageState === "about" && loadState === false && index === columns * (3 + Math.ceil((rows - 4) / 2)) + 1 ? 
+                    
+                    <div 
+                        style={{
+                            width:`${document.body.clientWidth - sizeMultiplier * 2 - 2}px`,
+                            height:`${document.body.clientWidth >= 560 ? sizeMultiplier * ((rows-4)/2 - 0.75) : sizeMultiplier * ((rows-4)/2 - 0.75)}px`,
+                            overflowY: 'scroll'
+                        }}
+                        className={`${textVanish ? 'about-animateDisappear-0ms' : "about-delay-0ms"} aboutbox text-white font-["Rajdhani"]  tracking-normal text-sm md:text-md opacity-100 top-[50%] left-[0%] bg-[rgb(20,20,20,0.8)] pl-[18px] border-[var(--g3)] border-[1px] rounded-sm ${document.body.clientWidth <= 560 ? `translate-y-[-16px]` : `translate-y-[-26px]`} p-5 absolute overflow-visible`}
+                    >
+                        <p className={`uppercase font-['Rajdhani'] tracking-wider text-xl md:text-2xl font-black pb-4`}>&lt; Timeline /&gt;</p>                         
+                        <ul className='flex flex-col gap-5'>
+                            <div className='grid grid-cols-[4fr_1fr_4fr] md:grid-cols-[4fr_1fr_8fr]'>
+                                <div className='text-end'>
+                                    <p className={`uppercase font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[500] -pb-2`}>Software Engineering Associate</p>                         
+                                    <p className={` font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[400] text-[var(--g3)]`}>@ Lloyds Banking Group</p>                         
+                                    <p className={`uppercase font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[400] pb-2 text-gray-300`}>Mar 2023 - Present</p>                         
+                                </div>
+                                <div>
+                                    <div className='timelineComp mx-auto my-auto bg-gradient-to-tl from-[var(--g3)] to-[var(--g4)] w-[15px] md:w-[25px] h-[15px] md:h-[25px] rounded-full content-none'></div>
+                                    <div className='mx-auto my-auto -mt-1 bg-gradient-to-b from-[var(--g3)] via-[#00000000] w-[1px] md:w-[2px] h-full '></div>
+                                </div>
+                                <div>
+                                    <ul className='tracking-normal'>
+                                        <li className='timelineListItem'>
+
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='grid grid-cols-[4fr_1fr_4fr] md:grid-cols-[4fr_1fr_8fr]'>
+                                <div className='text-end'>
+                                    <p className={`uppercase font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[500] -pb-2`}>Trainee Software Engineer</p>                         
+                                    <p className={` font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[400] text-[var(--g3)]`}>@ Bright Network Technology Academy</p>                         
+                                    <p className={`uppercase font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[400] pb-2 text-gray-300`}>Nov 2022 - Feb 2023</p>                         
+                                </div>
+                                <div>
+                                    <div className='timelineComp mx-auto my-auto bg-gradient-to-tl from-[var(--g3)] to-[var(--g4)] w-[15px] md:w-[25px] h-[15px] md:h-[25px] rounded-full content-none'></div>
+                                    <div className='mx-auto my-auto -mt-1 bg-gradient-to-b from-[var(--g3)] via-[#00000000] w-[1px] md:w-[2px] h-full '></div>
+
+                                </div>
+                                <div>
+                                    <ul className='ml-3 text-sm md:text-md flex flex-col gap-3'>
+                                        <li className='timelineListItem list-disc text-sm md:text-lg'>
+                                            Underwent 12-weeks of intensive training for back-end and front-end development. Learned core programming principles.
+                                        </li>
+                                        <li className='timelineListItem list-disc text-sm md:text-lg'>
+                                            Used Agile methodologies and test-driven development (TDD).
+                                        </li>
+                                        <li className='timelineListItem list-disc text-sm md:text-lg'>
+                                            Developed full-stack capstone project - a browser-based Pok&eacute;mon card-battle game.
+                                        </li>
+                                        <li className='timelineListItem list-disc text-sm md:text-lg'>
+                                            Developed a front-end movie review website using a pre-existing API, allowing users to login/sign-up and leave star-rating reviews on movies.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='grid grid-cols-[4fr_1fr_4fr] md:grid-cols-[4fr_1fr_8fr]'>
+                                <div className='text-end'>
+                                    <p className={`uppercase font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[500] -pb-2`}>Masters' in Mechanical Engineering</p>                         
+                                    <p className={` font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[400] text-[var(--g3)]`}>@ Imperial College London</p>                         
+                                    <p className={`uppercase font-['Rajdhani'] tracking-wider text-md md:text-lg md:leading-[-20px] font-[400] pb-2 text-gray-300`}>Oct 2018 - July 2022</p>                         
+                                </div>
+                                <div>
+                                    <div className='timelineComp mx-auto my-auto bg-gradient-to-tl from-[var(--g3)] to-[var(--g4)] w-[15px] md:w-[25px] h-[15px] md:h-[25px] rounded-full content-none'></div>
+                                    <div className='mx-auto my-auto -mt-1 bg-gradient-to-b from-[var(--g3)] via-[#00000000] w-[1px] md:w-[2px] h-full '></div>
+
+                                </div>
+                                <div>
+                                    <ul className='ml-3 text-sm md:text-md flex flex-col gap-3'>
+                                        <li className='timelineListItem list-disc text-sm md:text-lg'>
+                                            Developed an interactive web-app for visualising 2-degree-of-freedom oscillatory systems, allowing user input and graphically displaying system responses.
+                                        </li>
+                                        <li className='timelineListItem list-disc text-sm md:text-lg'>
+                                            Used Embedded C with a microcontroller to develop an electronic vehicle with collision-avoidance, via colour detection and turning at specific angles.
+                                        </li>                                       
+                                    </ul>
+                                </div>
+                            </div>
+                            
+
+                        </ul>
                     </div>
                     
                     
